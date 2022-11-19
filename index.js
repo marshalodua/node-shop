6adf2b18575bf20483a26bdbbce96972ec769ece
marshalodua <marshal.od.ua@gmail.com>
2022-11-19 02:45:53 +0200

    fix index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express       = require('express');
 const app           = express();
 const cors          = require('cors');
+const router        = require('./src/routes/index');
 
 const port          = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log(`Server is running: ${port}`);
+});
